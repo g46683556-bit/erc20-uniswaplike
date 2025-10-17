bd4bba5212bd3b39780cad2eed44ee8e3c32ddb3
g46683556-bit <g46683556@gmail.com>
2025-10-17 14:14:58 -0500

    Segunda carga
    Segunda carga

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,12 @@
 async function main() {
   const [deployer] = await ethers.getSigners();
-  console.log("Deploying with:", deployer.address);
-
-  const name = "UniswapLike Token";
-  const symbol = "ULT";
-  const initialSupply = "1000000"; // 1,000,000
-  const decimals = 18;
+  console.log("Desplegando con la cuenta:", deployer.address);
 
   const Token = await ethers.getContractFactory("UniswapLikeToken");
-  const token = await Token.deploy(name, symbol, initialSupply, decimals);
+  const token = await Token.deploy("UniswapLike Token", "ULT", 1000000, 18);
   await token.deployed();
 
-  console.log("Token deployed to:", token.address);
+  console.log("Token desplegado en:", token.address);
 }
 
 main().catch((error) => {
